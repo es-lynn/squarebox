189ea459cb965755af09f39ad061b4432601a5fa
Ariel Tay <atay@palo-it.com>
2021-07-29 12:05:47 +0800

    Setup camera

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 `yarn setup`
 
+If this does not work, try:
+```
+cd frontend 
+yarn install
+
+cd backend
+yarn install
+```
+
 # Running 
 
 `yarn start:frontend`
@@ -13,3 +22,4 @@
 1. `yarn compile:fix`
 2. Fix all errors & warnings
 3. Commit
+
--- frontend/src/pages/setup/2b_SetupScannerMode.tsx
+++ frontend/src/pages/setup/2b_SetupScannerMode.tsx
@@ -1,6 +1,21 @@
+import Button from 'antd/es/button'
 import React from 'react'
-import Text from 'antd/es/typography/Text'
-
+import Webcam from 'react-webcam'
 export const SetupScannerModePage = () => {
-  return <Text>WIP: Coming soon</Text>
+  const webcamRef = React.useRef<Webcam>(null)
+  const [imgSrc, setImgSrc] = React.useState<any>('')
+
+  const capture = React.useCallback(() => {
+    if (null !== webcamRef.current) {
+      const imageSrc: any = webcamRef.current.getScreenshot()
+      setImgSrc(imageSrc)
+    }
+  }, [webcamRef, setImgSrc])
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', width: '300px', gap: '20px' }}>
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      <Button onClick={capture}>Open Camera</Button>
+      {imgSrc && <img src={imgSrc} />}
+    </div>
+  )
 }
