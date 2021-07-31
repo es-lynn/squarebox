ad9dd821d5013752f50b5d689ef6b93bdeaaaec0
kenlojt <kenlojt@gmail.com>
2021-07-31 10:52:29 +0800

    [Ken] Added QRCode reader (#3)
    Co-authored-by: Kenlo <kenneth_lo@tech.gov.sg>

--- frontend/src/pages/setup/2b_SetupScannerMode.tsx
+++ frontend/src/pages/setup/2b_SetupScannerMode.tsx
@@ -1,9 +1,12 @@
 import Button from 'antd/es/button'
 import React from 'react'
 import Webcam from 'react-webcam'
+// import QrScanner from 'qr-scanner'
+import QrReader from 'react-qr-reader'
 export const SetupScannerModePage = () => {
   const webcamRef = React.useRef<Webcam>(null)
   const [imgSrc, setImgSrc] = React.useState<any>('')
+  const [text, setText] = React.useState("")
 
   const capture = React.useCallback(() => {
     if (null !== webcamRef.current) {
@@ -11,11 +14,33 @@ export const SetupScannerModePage = () => {
       setImgSrc(imageSrc)
     }
   }, [webcamRef, setImgSrc])
+  const videoElem = (<video></video>)
+  const handleError = (err: any) => {
+    console.log(err)
+  }
+  const login = async (hash: string | null) => {
+    console.log("scanning....")
+    setText(hash || "")
+  }
+  // const qrScanner = new QrScanner(videoElem, result => console.log('decoded qr code:', result))
   return (
     <div style={{ display: 'flex', flexDirection: 'column', width: '300px', gap: '20px' }}>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      {/* <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <Button onClick={capture}>Open Camera</Button>
-      {imgSrc && <img src={imgSrc} />}
+      {imgSrc && <img src={imgSrc} />} */}
+      {/* {qrScanner.start()} */}
+      <QrReader
+        delay={1000}
+        onError={handleError}
+        onScan={login}
+        style={{ width: '100%' }}
+      />
+      <div>
+        The word is:
+      </div>
+      <div>
+        {text}
+      </div>
     </div>
   )
 }
