287df61feed1f855e27b530a045a22866a300fcf
Ariel Tay <atay@palo-it.com>
2021-08-02 16:40:10 +0800

    Fix alignment issues (Temp)

--- frontend/src/pages/setup/2b_SetupScannerMode.tsx
+++ frontend/src/pages/setup/2b_SetupScannerMode.tsx
@@ -6,7 +6,7 @@ import QrReader from 'react-qr-reader'
 export const SetupScannerModePage = () => {
   const webcamRef = React.useRef<Webcam>(null)
   const [imgSrc, setImgSrc] = React.useState<any>('')
-  const [text, setText] = React.useState("")
+  const [text, setText] = React.useState('')
 
   const capture = React.useCallback(() => {
     if (null !== webcamRef.current) {
@@ -14,33 +14,34 @@ export const SetupScannerModePage = () => {
       setImgSrc(imageSrc)
     }
   }, [webcamRef, setImgSrc])
-  const videoElem = (<video></video>)
   const handleError = (err: any) => {
     console.log(err)
   }
   const login = async (hash: string | null) => {
-    console.log("scanning....")
-    setText(hash || "")
+    console.log('scanning....')
+    if (hash) {
+      setText(hash)
+    }
   }
+
   // const qrScanner = new QrScanner(videoElem, result => console.log('decoded qr code:', result))
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', width: '300px', gap: '20px' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '600px',
+        gap: '10px',
+        alignItems: 'center'
+      }}
+    >
       {/* <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <Button onClick={capture}>Open Camera</Button>
       {imgSrc && <img src={imgSrc} />} */}
       {/* {qrScanner.start()} */}
-      <QrReader
-        delay={1000}
-        onError={handleError}
-        onScan={login}
-        style={{ width: '100%' }}
-      />
-      <div>
-        The word is:
-      </div>
-      <div>
-        {text}
-      </div>
+      <QrReader delay={1000} onError={handleError} onScan={login} style={{ width: '100%' }} />
+      <h1>The word is:</h1>
+      <h3>{text}</h3>
     </div>
   )
 }
