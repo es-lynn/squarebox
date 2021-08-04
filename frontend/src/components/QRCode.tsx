279834495fe486158160effd5d4789df4da7c860
Kenlo <kenneth_lo@tech.gov.sg>
2021-08-04 17:55:07 +0800

    [Ken] Video

--- frontend/src/components/QRCode.tsx
+++ frontend/src/components/QRCode.tsx
@@ -11,13 +11,33 @@ const TextAreaCustom = styled(TextArea)`
 
 export const QRCodeComponent = () => {
   const [valueToQr, setValueToQR] = useState('')
+  const [textValue, setTextValue] = useState('')
   const [showQRGenerator, setShowQRGenerator] = useState(false)
+  const textLengthRegex = /.{1,100}/g //adjust the max character to get the QR code size
+  const delay = (ms: number) => new Promise(res => setTimeout(res, ms))
+  const videoFrameRateInMS = 1000
   const onInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setValueToQR(e.target.value)
+    setTextValue(e.target.value)
   }
 
-  const generateQR = () => {
+  const generateQR = async () => {
+    const strArray = textValue.match(textLengthRegex)
     setShowQRGenerator(true)
+    if (!strArray) return
+    for (let i = -1; i < strArray.length + 1; i++) {
+      if (i === -1) {
+        setValueToQR(strArray.length + '--||SQUAREBOXSTART||--' + strArray.length)
+      } else if (i === strArray.length) {
+        setValueToQR(+strArray.length + '--||SQUAREBOXEND||--' + strArray.length)
+      } else {
+        setValueToQR(strArray[i])
+      }
+      await delay(videoFrameRateInMS)
+
+      if (i === strArray.length) {
+        i = -2
+      }
+    }
   }
 
   return (
@@ -27,7 +47,7 @@ export const QRCodeComponent = () => {
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => onInputChange(e)}
         showCount
       />
-      <Button disabled={!valueToQr} onClick={generateQR}>
+      <Button disabled={!textValue} onClick={generateQR}>
         Generate QR
       </Button>
       <br />
