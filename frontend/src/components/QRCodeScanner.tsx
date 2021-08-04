5b67b4de911d35b9cecd67d802f71026f495d806
Jaslyn S <jaslyn_soh@tech.gov.sg>
2021-08-05 05:05:30 +0800

    fixed some idiot's code (ie. me)

--- frontend/src/components/QRCodeScanner.tsx
+++ frontend/src/components/QRCodeScanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Input } from 'native-base'
 import { useLinkedState } from '../lib/LinkedState'
 import { receivingStore } from '../pages/temp/receiving/ReceivingStore'
@@ -10,7 +10,7 @@ export type QRCodeScannerProps = {
 }
 // TODO: Replace with a real QRCode scanner
 export const QRCodeScanner = ({ size = 256, onQRCodeScanned }: QRCodeScannerProps) => {
-  const [content, setContent] = useLinkedState(receivingStore)
+  const [content, setContent] = useState<string>('')
 
   const handleError = (err: any) => {
     console.error(err)
