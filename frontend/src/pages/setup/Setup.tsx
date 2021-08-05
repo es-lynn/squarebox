78e5481311b249322c9c7bc6859d96afcb531cf4
Ariel Tay <shihqian@gmail.com>
2021-08-05 16:23:49 +0800

    New layout (#22)
    * Add common layout and update login page
    
    * Update setup page
    
    Co-authored-by: Ariel Tay <“atay@palo-it.com”>

--- frontend/src/pages/setup/Setup.tsx
+++ frontend/src/pages/setup/Setup.tsx
@@ -2,13 +2,33 @@ import { PageView } from '../../components/business/PageView'
 import { Button, Text } from 'native-base'
 import React from 'react'
 import { configureDevice } from './Setup.func'
+import styled from 'styled-components/native'
+import { BodyText, HeaderText, TwoButtonGrid, PrimaryButton } from '../../style/style'
+import ChooseDevice from '../../images/choose_device.svg'
+import styledHtml from 'styled-components'
+
+const ChooseDeviceImage = styledHtml.img`
+  width: 331px;
+  height: 255px;
+  margin-top: 7px;
+`
+
+const ParaText = styled(BodyText)`
+  margin-top: 41px;
+  margin-bottom: 16px;
+`
 
 export const Setup = () => {
   return (
     <PageView>
-      <Text>Select your device type:</Text>
-      <Button onPress={() => configureDevice('computer')}>Computer</Button>
-      <Button onPress={() => configureDevice('scanner')}>Scanner</Button>
+      <HeaderText>Almost there!</HeaderText>
+      <HeaderText> Now, help us choose a device type. 🎮</HeaderText>
+      <ChooseDeviceImage src={ChooseDevice} alt="ChooseDevice" />
+      <ParaText>Select your device type:</ParaText>
+      <TwoButtonGrid>
+        <PrimaryButton onPress={() => configureDevice('computer')}>Computer</PrimaryButton>
+        <PrimaryButton onPress={() => configureDevice('scanner')}>Scanner</PrimaryButton>
+      </TwoButtonGrid>
     </PageView>
   )
 }
