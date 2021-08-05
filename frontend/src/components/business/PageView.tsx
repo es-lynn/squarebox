0783a13241dc17945d7e49fa592c791812690b67
Ariel Tay <atay@palo-it.com>
2021-08-05 14:02:42 +0800

    Add common layout and update login page

--- frontend/src/components/business/PageView.tsx
+++ frontend/src/components/business/PageView.tsx
@@ -19,12 +19,16 @@ export const PageView = ({ children, style, containerStyle, ...rest }: PageViewP
 }
 
 export const BackgroundView = withStyle(View)({
-  maxWidth: '100%'
+  maxWidth: '100%',
+  height: '100vh'
 })
 
 export const ContainerView = withStyle(View)({
   margin: 'auto',
   width: '100%',
   padding: '0 16',
-  maxWidth: 768
+  maxWidth: 768,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
 })
--- frontend/src/style/style.ts
+++ frontend/src/style/style.ts
@@ -1,3 +1,9 @@
+import { Button, Text } from 'native-base'
+import { Input as AntInput } from 'antd'
+import styled from 'styled-components/native'
+import styledHtml from 'styled-components'
+import 'antd/dist/antd.css'
+
 export const sp = {
   _4: 4,
   _8: 8,
@@ -14,3 +20,94 @@ export const cl = {
   white: '#FFF',
   black: '#000'
 }
+
+export const PrimaryButton = styled(Button)`
+  background-color: #6842ff;
+  width: 277px;
+  height: 52px;
+  font-size: 16px;
+  border-radius: 100px;
+  font-weight: bold;
+`
+
+export const TwoButtonGrid = styledHtml.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+export const HeaderText = styled(Text)`
+  font-family: Open Sans;
+  font-weight: 700;
+  text-align: center;
+  font-size: 30px;
+  line-height: 40.85px;
+`
+
+export const BodyText = styled(Text)`
+  font-family: Open Sans;
+  font-weight: 600;
+  text-align: center;
+  font-size: 16px;
+  line-height: 21.79px;
+`
+
+export const PhoneInput = styledHtml(AntInput)`
+width: 307px;
+height: 42px;
+  .ant-input-group {
+    border: 1.5px solid #9FB0FE;
+    border-radius: 100px;
+  }
+
+  .ant-input::placeholder {
+    color: #92929D;
+  }
+
+  .ant-input-group-addon {
+    border-radius: 100px;
+    font-family: Open Sans;
+    font-weight: 600;
+    text-align: center;
+    font-size: 16px;
+    line-height: 21.79px;
+    color:#171725;
+    background-color: #FFF;
+    border: none;
+    padding: 10px 20px 10px 20px;
+  }
+
+  .ant-input {
+    font-family: Open Sans;
+    font-size: 16px;
+    border-radius: 100px;
+    height:42px;
+    border-left: 1px solid #9FB0FE;
+  }
+`
+
+export const PassWordInput = styledHtml(AntInput.Password)`
+background: #FFFFFF;
+border: 1.5px solid #9FB0FE;
+border-radius: 100px;
+height: 48px;
+padding-top: 0;
+
+.ant-input-prefix {
+  padding: 10px;
+}
+
+.ant-input {
+  height: 45px;
+  background-color: #fff;
+}
+`
+
+export const CustomInput = styledHtml(AntInput)`
+  border: 1.5px solid #9fb0fe;
+  border-radius: 100px;
+  height: 48px;
+  padding-top:10px;
+  padding-bottom: 10px;
+  padding-left: 16px;
+`
